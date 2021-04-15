bcaa9cbede4676c7c5946328a3c68b056ee3f110
SedaII <i.messnaoui@gmail.com>
2021-04-15 16:05:11 +0200

    Fixed TS errors & created Physics Fn in the scene

--- src/characters/player.js
+++ src/characters/player.js
@@ -12,34 +12,31 @@ export default class Player extends Phaser.Physics.Arcade.Sprite
         this.Q = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
         this.D = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.SPACE = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
+        
     }
+
+    init()
+    {}
 
     update()
     {
         if(this.scene.cursors.left.isDown) {
-            // @ts-ignore
-            this.body.setVelocityX(-100);
+            this.setVelocityX(-100);
         } else if (this.scene.cursors.right.isDown) {
-            // @ts-ignore
-            this.body.setVelocityX(100);
+            this.setVelocityX(100);
         } else if(this.Q.isDown) {
-            // @ts-ignore
-            this.body.setVelocityX(-100);
+            this.setVelocityX(-100);
         } else if (this.D.isDown) {
-            // @ts-ignore
-            this.body.setVelocityX(100);
+            this.setVelocityX(100);
         } else {
-            // @ts-ignore
-            this.body.setVelocityX(0);
+            this.setVelocityX(0);
         }
         
         if (this.scene.cursors.up.isDown && this.body.touching.down) {
-            // @ts-ignore
-            this.body.setVelocityY(-200);
+            
+            this.setVelocityY(-200);
         } else if (this.SPACE.isDown && this.body.touching.down) {
-            // @ts-ignore
-            this.body.setVelocityY(-200);
+            this.setVelocityY(-200);
         }
     }
   }
--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -6,6 +6,7 @@ export default class Level1 extends Phaser.Scene
 	constructor()
 	{
 		super('level1');
+
 	}
 
 	preload()
@@ -15,15 +16,20 @@ export default class Level1 extends Phaser.Scene
 
     create()
     {
-        let ground = this.add.rectangle(0, 560, 800, 40, 0x6666ff).setOrigin(0,0);
+        this.createPhysics()
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+        
+    }
+
+    createPhysics()
+    {
+        const ground = this.add.rectangle(0, 560, 800, 40, 0x6666ff).setOrigin(0,0);
         this.physics.add.staticGroup(ground);
 
         this.player = new Player(this, 400, 300, 'dude', '4');
 
         this.physics.add.collider(this.player, ground);
-
-        this.cursors = this.input.keyboard.createCursorKeys();
-        
     }
 
     update()
